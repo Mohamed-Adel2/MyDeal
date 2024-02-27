6ba797a5e9781f81119ecd3a6ee719200533062f
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-02-27 06:24:01 +0200

    Update DB_schema.sql

--- DB_schema.sql
+++ DB_schema.sql
@@ -1,6 +1,6 @@
 CREATE database mydeal;
 CREATE TABLE Customers (
-    customer_id INT PRIMARY KEY,
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255),
     email VARCHAR(255),
     phone_number VARCHAR(20),
@@ -12,7 +12,7 @@ CREATE TABLE Customers (
 );
 
 CREATE TABLE Products (
-    product_id INT PRIMARY KEY,
+    product_id INT AUTO_INCREMENT PRIMARY KEY,
     product_name VARCHAR(255),
     description TEXT,
     category VARCHAR(50),
@@ -22,7 +22,7 @@ CREATE TABLE Products (
 );
 
 CREATE TABLE Orders (
-    order_id INT PRIMARY KEY,
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
@@ -47,14 +47,14 @@ CREATE TABLE CustomerCart (
 );
 
 CREATE TABLE ProductImages (
-    image_id INT PRIMARY KEY,
+    image_id INT AUTO_INCREMENT PRIMARY KEY,
     product_id INT,
     image BLOB,
     FOREIGN KEY (product_id) REFERENCES Products(product_id)
 );
 
 CREATE TABLE Reviews (
-    review_id INT PRIMARY KEY,
+    review_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     product_id INT,
     stars INT,
