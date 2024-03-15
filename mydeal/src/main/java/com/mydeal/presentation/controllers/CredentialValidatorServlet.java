d3d9a7b18d3717fe72aebffc7fffd57faf555a67
Mohamad amgad elsaid <mohamadamgad09@gmail.com>
2024-03-15 10:00:15 +0200

    Register Validation Code Added - bug in phone Validation from server

--- mydeal/src/main/java/com/mydeal/presentation/controllers/CredentialValidatorServlet.java
+++ mydeal/src/main/java/com/mydeal/presentation/controllers/CredentialValidatorServlet.java
@@ -14,15 +14,20 @@ import java.io.IOException;
 public class CredentialValidatorServlet extends HttpServlet {
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Validation Request Received");
         String email = req.getParameter(RequestKey.RQ_CustomerEmail);
         String phoneNumber = req.getParameter(RequestKey.RQ_CustomerPhoneNumber);
         CustomerDataValidatorService customerDataValidatorService = new CustomerDataValidatorService();
-        String ret = "invalid";
+        String ret = "";
         if (email != null) {
             ret = customerDataValidatorService.validateEmail(email) ? "valid" : "invalid";
         } else if (phoneNumber != null) {
             ret = customerDataValidatorService.validatePhone(phoneNumber) ? "valid" : "invalid";
         }
+        // Set response content type to JSON
+        resp.setContentType("application/json");
+        // Set response character encoding (optional)
+        resp.setCharacterEncoding("UTF-8");
         resp.getWriter().write(ret);
     }
 }
