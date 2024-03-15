7e5186b18de99c3b960c917730b2b626a0a94bc2
Mohamad amgad elsaid <mohamadamgad09@gmail.com>
2024-03-15 09:27:45 +0200

    Register code validation implemented

--- mydeal/src/main/java/com/mydeal/domain/util/RequestKey.java
+++ mydeal/src/main/java/com/mydeal/domain/util/RequestKey.java
@@ -8,7 +8,7 @@ public class RequestKey {
     public static final String RQ_CustomerPhoneNumber = "phone_number";
     public static final String RQ_CustomerDOB = "date_of_birth";
     public static final String RQ_CustomerCreditLimit = "credit_limit";
-    public static final String RQ_CustomerStreet = "address";
+    public static final String RQ_CustomerStreet = "street";
     public static final String RQ_CustomerCity = "city";
     public static final String RQ_CustomerApartment = "apartment";
 }
