6eecef925e3496943cea86ed8a0aacd6090e281d
Mohamad amgad elsaid <mohamadamgad09@gmail.com>
2024-03-19 14:39:14 +0200

    Changes in Login and Update Info Implemented
    Storing Credentials Locally Implemented , Logging Logic Modified , show data in update info page done .

--- mydeal/src/main/java/com/mydeal/presentation/controllers/LoginServlet.java
+++ mydeal/src/main/java/com/mydeal/presentation/controllers/LoginServlet.java
@@ -1,6 +1,8 @@
 package com.mydeal.presentation.controllers;
 
+import com.google.gson.Gson;
 import com.mydeal.domain.entities.Customer;
+import com.mydeal.domain.mapping.CustomerMapping;
 import com.mydeal.domain.services.CustomerLoginService;
 import com.mydeal.domain.util.RequestKey;
 import com.mydeal.presentation.utils.ClientJSAlert;
@@ -8,6 +10,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.hibernate.Hibernate;
 
 import java.io.IOException;
 import java.util.Optional;
@@ -20,24 +23,28 @@ public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        if (req.getSession(false) != null)
+        if (req.getSession(false) != null) {
             resp.sendRedirect("index.html");
-        else
+        } else
             resp.sendRedirect("login.html");
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html");
-
         String email = req.getParameter(RequestKey.RQ_CustomerEmail);
         String password = req.getParameter(RequestKey.RQ_CustomerPassword);
         CustomerLoginService customerLoginService = new CustomerLoginService();
         Optional<Customer> loggedCustomer = Optional.ofNullable(customerLoginService.login(email, password));
         if (loggedCustomer.isPresent()) {
+            resp.setContentType("application/json"); // Set content type to JSON
+            resp.setCharacterEncoding("UTF-8");
             Customer customer = loggedCustomer.get();
+            // Initialize the address association explicitly
+            // Hibernate.initialize(customer.getAddress());
+            String jsonCustomer = new Gson().toJson(CustomerMapping.convertEntityToModel(customer));
             req.getSession(true).setAttribute("user", customer);
-            resp.sendRedirect("index.html");
+            // Set JSON string as response content
+            resp.getWriter().write(jsonCustomer);
         } else {
             resp.setContentType("text/html");
             ClientJSAlert.showAlert(resp, "Invalid Email or Password");
---
+++ mydeal/src/main/java/com/mydeal/presentation/controllers/Navigator.java
@@ -0,0 +1,20 @@
+package com.mydeal.presentation.controllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class Navigator extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        String destination = req.getParameter("page");
+        System.out.println("navigation to " + destination);
+        if (destination != null) {
+            resp.sendRedirect(destination);
+        }
+    }
+}
